c6b264235eeeacc2996e53511d231c4fde476f3b
Kevin Loughrey <kevin@codeinstitute.net>
2024-05-20 12:01:34 +0000

    remove commented out code

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -22,10 +22,6 @@ let hero = {
     ],
     attack() {
         return Math.ceil(Math.random() * 10);
-        /* console.log(
-           `"${this.attacks[attack].dialogue}," shouted Oliver, as he used his ${this.attacks[attack].name} attack and dealt ${damage} damage.\n`,
-         );*/
-        //    return damage;
     },
     die() {
         fightStatusElement.innerText = `Mighty Hulk picked up Oliver's frail frame and tore the poor little boy in half. "HULK WIIIIINNNNSSSS," he roared.`;
@@ -127,20 +123,3 @@ function playSound(fileName) {
     let sound = new Audio(`assets/sounds/${fileName}.mp3`);
     sound.play();
 }
-
-/*  let gameRunning = true;
-  while (gameRunning) {
-    let damageToChar2 = char1.attack();
-    char2.health -= damageToChar2;
-    let damageToChar1 = char2.attack();
-    char1.health -= damageToChar1;
-  
-    if (char1.health <= 0) {
-      char1.die();
-      gameRunning = false;
-    } else if (char2.health <= 0) {
-      char2.die();
-      gameRunning = false;
-    }
-  }
-  */
